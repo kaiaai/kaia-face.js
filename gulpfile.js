af60975233033efe9ec83177c3b3ace1214c4195
Ilia Ovsiannikov <ilia@oomwoo.com>
2018-12-16 04:58:40 -0800

    Restored

--- gulpfile.js
+++ gulpfile.js
@@ -1,76 +1,77 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var changed = require('gulp-changed');
-var plumber = require('gulp-plumber');
-//var uglify = require('gulp-uglify-es').default;
-var uglify = require('gulp-uglify');
-//var uglify = require('gulp-minify');
-var concat = require('gulp-concat');
-var umd = require('gulp-umd');
-var rename = require('gulp-rename');
-var clean = require('gulp-clean');
-var watch = require('gulp-watch');
-var batch = require('gulp-batch');
-//var babel = require('gulp-babel');
+const gulp = require('gulp');
+const jshint = require('gulp-jshint');
+const changed = require('gulp-changed');
+const plumber = require('gulp-plumber');
+//const uglify = require('gulp-uglify-es').default;
+const uglify = require('gulp-uglify');
+//const uglify = require('gulp-minify');
+const concat = require('gulp-concat');
+const umd = require('gulp-umd');
+const rename = require('gulp-rename');
+const clean = require('gulp-clean');
+const watch = require('gulp-watch');
+const batch = require('gulp-batch');
+//const babel = require('gulp-babel');
 const imagemin = require('gulp-imagemin');
 const inject = require('gulp-js-base64-inject');
 
-const VER = '-0.1.0';
 const LIB_NAME = 'kaia-face.js';
-const SRC = './src/js/';
-const SRCJS = [SRC+'helpers.js', SRC+'animationModule.js',
-  SRC+'animationEffectModule.js', SRC+'main.js', SRC+'user_api.js',
-  SRC+'expression.js', SRC+'expressionElement.js'];
-const DIST = 'dist/';
-const BUNDLE = 'bundle/';
-const BASE64 = 'base64/';
-//const IMG = 'img';
-const JS = '*.js';
-
-var SRC_JS = SRC + JS;
-var DIST_JS = './' + DIST + JS;
+const UMD_EXPORTS = 'Face';
+const UMD_NAMESPACE = UMD_EXPORTS;
+const SRC_DIR = 'src/';
+const SRC_JS_DIR = SRC_DIR + 'js/';
+const IMG_DIR = 'img/';
+const SRC_JS = [SRC_JS_DIR + 'helpers.js', SRC_JS_DIR + 'animationModule.js',
+  SRC_JS_DIR + 'animationEffectModule.js', SRC_JS_DIR + 'main.js',
+  SRC_JS_DIR + 'user_api.js', SRC_JS_DIR + 'expression.js',
+  SRC_JS_DIR + 'expressionElement.js'];
+const DIST_DIR = 'dist/';
+const BUNDLE_DIR = 'bundle/';
+const BASE64_DIR = 'base64/';
 
 gulp.task('default', ['minify']);
 
-gulp.task('base64', ['concat', 'imagemin'], function() {
-    return gulp.src(BUNDLE + LIB_NAME)
-        .pipe(inject({
-            basepath: BUNDLE + 'img/',
-            pattern: /['"]\/uploads\/oomwoo\/face\/img\/([a-zA-Z0-9\-_.\\/]+)['"]/g
-            //debug: true
-        }))
-        .pipe(gulp.dest(BASE64));
+gulp.task('base64', ['concat', 'imagemin'], () => {
+  gulp.src(BUNDLE_DIR + LIB_NAME)
+  .pipe(inject({
+    basepath: BUNDLE_DIR + 'img/',
+    pattern: /['"]\/uploads\/oomwoo\/face\/img\/([a-zA-Z0-9\-_.\\/]+)['"]/g
+    //pattern: /['"]([a-zA-Z0-9\-_\\/]+.png)['"]/g
+    //debug: true
+  }))
+  .pipe(gulp.dest(BASE64_DIR));
 });
 
 gulp.task('imagemin', ['clean'], () =>
-	gulp.src('src/img/**/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest(BUNDLE + 'img'))
+  gulp.src(SRC_DIR + IMG_DIR + '**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest(BUNDLE_DIR + IMG_DIR))
 );
 
-gulp.task('watch', function () {
-    watch(SRC_JS, batch(function (events, done) {
-        gulp.start('default', done);
-    }));
+gulp.task('watch', () => {
+  watch(SRC_JS, batch((events, done) => {
+    gulp.start('default', done);
+  }));
 });
 
-gulp.task('clean', function () {
-    return gulp.src([DIST, BUNDLE], {read: false})
-      .pipe(clean())
+gulp.task('clean', () => {
+  gulp.src([DIST_DIR, BUNDLE_DIR, BASE64_DIR], {read: false})
+  .pipe(clean())
 });
 
-gulp.task('umd', ['jshint-base64'], function() {
-  return gulp.src(BASE64 + LIB_NAME)
+gulp.task('umd', [], () => {
+//gulp.task('umd', ['jshint-base64'], () => {
+  gulp.src(BASE64_DIR + LIB_NAME)
 //    .pipe(babel({ presets: ['es2015'] }))
-    .pipe(plumber())
-    .pipe(umd())
-    .pipe(rename({
-      suffix: VER,
-    }))
-    .pipe(gulp.dest(DIST));
+  .pipe(plumber())
+  .pipe(umd({
+    namespace: () => UMD_NAMESPACE,
+    exports: () => UMD_EXPORTS,
+  }))
+  .pipe(gulp.dest(DIST_DIR));
 });
 
-gulp.task('jshint-src', ['clean'], function() {
+gulp.task('jshint-src', ['clean'], () => {
   gulp.src(SRC_JS)
   .pipe(plumber())
 //  .pipe(babel({ presets: ['es2015'] }))
@@ -78,46 +79,48 @@ gulp.task('jshint-src', ['clean'], function() {
   .pipe(jshint.reporter('default'));
 });
 
-gulp.task('jshint-bundle', ['concat'], function() {
-  gulp.src(BUNDLE + LIB_NAME)
+gulp.task('jshint-bundle', ['concat'], () => {
+  gulp.src(BUNDLE_DIR + LIB_NAME)
   .pipe(plumber())
 //  .pipe(babel({ presets: ['es2015'] }))
   .pipe(jshint({ esversion: 6 })) // { esversion: 6 }
   .pipe(jshint.reporter('default'));
 });
 
-gulp.task('jshint-base64', ['base64'], function() {
-  gulp.src(BASE64 + LIB_NAME)
+gulp.task('jshint-base64', ['base64'], () => {
+  gulp.src(BASE64_DIR + LIB_NAME)
   .pipe(plumber())
 //  .pipe(babel({ presets: ['es2015'] }))
   .pipe(jshint({ esversion: 6 })) // { esversion: 6 }
   .pipe(jshint.reporter('default'));
 });
 
-gulp.task('jshint-dist', ['minify'], function() {
-  gulp.src([DIST_JS, '!./' + DIST + '*.min.js'])
+gulp.task('jshint-umd', ['umd'], () => {
+  gulp.src(DIST_DIR + LIB_NAME)
   .pipe(plumber())
-  .pipe(jshint({ esversion: 6 }))
+//  .pipe(babel({ presets: ['es2015'] }))
+  .pipe(jshint({ esversion: 6 })) // { esversion: 6 }
   .pipe(jshint.reporter('default'));
 });
 
-gulp.task('minify', ['umd'], function() {
-  return gulp.src([DIST_JS, '!' + DIST + '*.min.js'])
+//gulp.task('minify', ['umd'], () => {
+gulp.task('minify', [], () => {
+  gulp.src(DIST_DIR + LIB_NAME)
   .pipe(uglify())
-  .pipe(rename(function (path) {
+  .pipe(rename((path) => {
     path.basename += '.min';
-   }))
-  .pipe(gulp.dest(DIST))
+  }))
+  .pipe(gulp.dest(DIST_DIR))
 });
 
-gulp.task('changed', function() {
-  return gulp.src(SRC_JS)
-  .pipe(changed(DIST))
-  .pipe(gulp.dest(DIST))
+gulp.task('changed', () => {
+  gulp.src(SRC_JS)
+  .pipe(changed(DIST_DIR))
+  .pipe(gulp.dest(DIST_DIR))
 });
 
-gulp.task('concat', ['clean'], function() {
-  return gulp.src(SRCJS)
+gulp.task('concat', ['clean'], () => {
+  gulp.src(SRC_JS)
   .pipe(concat(LIB_NAME))
-  .pipe(gulp.dest(BUNDLE))
+  .pipe(gulp.dest(BUNDLE_DIR))
 });
